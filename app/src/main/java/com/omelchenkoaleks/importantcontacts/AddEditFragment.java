206860b4282d5614b0979b0c16492d0010bf379c
omelchenkoaleks <omelchenkoaleks@gmail.com>
2018-12-24 08:47:05 +0200

    Версия 1.

--- app/src/main/java/com/omelchenkoaleks/importantcontacts/AddEditFragment.java
+++ app/src/main/java/com/omelchenkoaleks/importantcontacts/AddEditFragment.java
@@ -1,108 +1,271 @@
 package com.omelchenkoaleks.importantcontacts;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+
+import com.omelchenkoaleks.importantcontacts.data.DatabaseDescription;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AddEditFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AddEditFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AddEditFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class AddEditFragment extends Fragment
+        implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public AddEditFragment() {
-        // Required empty public constructor
+    // defines callback method implemented by MainActivity
+    public interface AddEditFragmentListener {
+        // called when contact is saved
+        void onAddEditCompleted(Uri contactUri);
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddEditFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddEditFragment newInstance(String param1, String param2) {
-        AddEditFragment fragment = new AddEditFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    // constant used to identify the Loader
+    private static final int CONTACT_LOADER = 0;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private AddEditFragmentListener listener; // MainActivity
+    private Uri contactUri; // Uri of selected contact
+    private boolean addingNewContact = true; // adding (true) or editing
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_edit, container, false);
-    }
+    // EditTexts for contact information
+    private TextInputLayout nameTextInputLayout;
+    private TextInputLayout phoneTextInputLayout;
+    private TextInputLayout emailTextInputLayout;
+    private TextInputLayout streetTextInputLayout;
+    private TextInputLayout cityTextInputLayout;
+    private TextInputLayout stateTextInputLayout;
+    private TextInputLayout zipTextInputLayout;
+    private FloatingActionButton saveContactFAB;
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+    private CoordinatorLayout coordinatorLayout; // used with SnackBars
 
+    // set AddEditFragmentListener when Fragment attached
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+        listener = (AddEditFragmentListener) context;
     }
 
+    // remove AddEditFragmentListener when Fragment detached
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+        listener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    // called when Fragment's view needs to be created
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        setHasOptionsMenu(true); // fragment has menu items to display
+
+        // inflate GUI and get references to EditTexts
+        View view =
+                inflater.inflate(R.layout.fragment_add_edit, container, false);
+        nameTextInputLayout =
+                (TextInputLayout) view.findViewById(R.id.nameTextInputLayout);
+        nameTextInputLayout.getEditText().addTextChangedListener(
+                nameChangedListener);
+        phoneTextInputLayout =
+                (TextInputLayout) view.findViewById(R.id.phoneTextInputLayout);
+        emailTextInputLayout =
+                (TextInputLayout) view.findViewById(R.id.emailTextInputLayout);
+        streetTextInputLayout =
+                (TextInputLayout) view.findViewById(R.id.streetTextInputLayout);
+        cityTextInputLayout =
+                (TextInputLayout) view.findViewById(R.id.cityTextInputLayout);
+        stateTextInputLayout =
+                (TextInputLayout) view.findViewById(R.id.stateTextInputLayout);
+        zipTextInputLayout =
+                (TextInputLayout) view.findViewById(R.id.zipTextInputLayout);
+
+        // set FloatingActionButton's event listener
+        saveContactFAB = (FloatingActionButton) view.findViewById(
+                R.id.saveFloatingActionButton);
+        saveContactFAB.setOnClickListener(saveContactButtonClicked);
+        updateSaveButtonFAB();
+
+        // used to display SnackBars with brief messages
+        coordinatorLayout = (CoordinatorLayout) getActivity().findViewById(
+                R.id.coordinatorLayout);
+
+        Bundle arguments = getArguments(); // null if creating new contact
+
+        if (arguments != null) {
+            addingNewContact = false;
+            contactUri = arguments.getParcelable(MainActivity.CONTACT_URI);
+        }
+
+        // if editing an existing contact, create Loader to get the contact
+        if (contactUri != null)
+            getLoaderManager().initLoader(CONTACT_LOADER, null, this);
+
+        return view;
     }
+
+    // detects when the text in the nameTextInputLayout's EditText changes
+    // to hide or show saveButtonFAB
+    private final TextWatcher nameChangedListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                                      int after) {}
+
+        // called when the text in nameTextInputLayout changes
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before,
+                                  int count) {
+            updateSaveButtonFAB();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) { }
+    };
+
+    // shows saveButtonFAB only if the name is not empty
+    private void updateSaveButtonFAB() {
+        String input =
+                nameTextInputLayout.getEditText().getText().toString();
+
+        // if there is a name for the contact, show the FloatingActionButton
+        if (input.trim().length() != 0)
+            saveContactFAB.show();
+        else
+            saveContactFAB.hide();
+    }
+
+    // responds to event generated when user saves a contact
+    private final View.OnClickListener saveContactButtonClicked =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // hide the virtual keyboard
+                    ((InputMethodManager) getActivity().getSystemService(
+                            Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(
+                            getView().getWindowToken(), 0);
+                    saveContact(); // save contact to the database
+                }
+            };
+
+    // saves contact information to the database
+    private void saveContact() {
+        // create ContentValues object containing contact's key-value pairs
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DatabaseDescription.Contact.COLUMN_NAME,
+                nameTextInputLayout.getEditText().getText().toString());
+        contentValues.put(DatabaseDescription.Contact.COLUMN_PHONE,
+                phoneTextInputLayout.getEditText().getText().toString());
+        contentValues.put(DatabaseDescription.Contact.COLUMN_EMAIL,
+                emailTextInputLayout.getEditText().getText().toString());
+        contentValues.put(DatabaseDescription.Contact.COLUMN_STREET,
+                streetTextInputLayout.getEditText().getText().toString());
+        contentValues.put(DatabaseDescription.Contact.COLUMN_CITY,
+                cityTextInputLayout.getEditText().getText().toString());
+        contentValues.put(DatabaseDescription.Contact.COLUMN_STATE,
+                stateTextInputLayout.getEditText().getText().toString());
+        contentValues.put(DatabaseDescription.Contact.COLUMN_ZIP,
+                zipTextInputLayout.getEditText().getText().toString());
+
+        if (addingNewContact) {
+            // use Activity's ContentResolver to invoke
+            // insert on the AddressBookContentProvider
+            Uri newContactUri = getActivity().getContentResolver().insert(
+                    DatabaseDescription.Contact.CONTENT_URI, contentValues);
+
+            if (newContactUri != null) {
+                Snackbar.make(coordinatorLayout,
+                        R.string.contact_added, Snackbar.LENGTH_LONG).show();
+                listener.onAddEditCompleted(newContactUri);
+            }
+            else {
+                Snackbar.make(coordinatorLayout,
+                        R.string.contact_not_added, Snackbar.LENGTH_LONG).show();
+            }
+        }
+        else {
+            // use Activity's ContentResolver to invoke
+            // insert on the AddressBookContentProvider
+            int updatedRows = getActivity().getContentResolver().update(
+                    contactUri, contentValues, null, null);
+
+            if (updatedRows > 0) {
+                listener.onAddEditCompleted(contactUri);
+                Snackbar.make(coordinatorLayout,
+                        R.string.contact_updated, Snackbar.LENGTH_LONG).show();
+            }
+            else {
+                Snackbar.make(coordinatorLayout,
+                        R.string.contact_not_updated, Snackbar.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    // called by LoaderManager to create a Loader
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // create an appropriate CursorLoader based on the id argument;
+        // only one Loader in this fragment, so the switch is unnecessary
+        switch (id) {
+            case CONTACT_LOADER:
+                return new CursorLoader(getActivity(),
+                        contactUri, // Uri of contact to display
+                        null, // null projection returns all columns
+                        null, // null selection returns all rows
+                        null, // no selection arguments
+                        null); // sort order
+            default:
+                return null;
+        }
+    }
+
+    // called by LoaderManager when loading completes
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // if the contact exists in the database, display its data
+        if (data != null && data.moveToFirst()) {
+            // get the column index for each data item
+            int nameIndex = data.getColumnIndex(DatabaseDescription.Contact.COLUMN_NAME);
+            int phoneIndex = data.getColumnIndex(DatabaseDescription.Contact.COLUMN_PHONE);
+            int emailIndex = data.getColumnIndex(DatabaseDescription.Contact.COLUMN_EMAIL);
+            int streetIndex = data.getColumnIndex(DatabaseDescription.Contact.COLUMN_STREET);
+            int cityIndex = data.getColumnIndex(DatabaseDescription.Contact.COLUMN_CITY);
+            int stateIndex = data.getColumnIndex(DatabaseDescription.Contact.COLUMN_STATE);
+            int zipIndex = data.getColumnIndex(DatabaseDescription.Contact.COLUMN_ZIP);
+
+            // fill EditTexts with the retrieved data
+            nameTextInputLayout.getEditText().setText(
+                    data.getString(nameIndex));
+            phoneTextInputLayout.getEditText().setText(
+                    data.getString(phoneIndex));
+            emailTextInputLayout.getEditText().setText(
+                    data.getString(emailIndex));
+            streetTextInputLayout.getEditText().setText(
+                    data.getString(streetIndex));
+            cityTextInputLayout.getEditText().setText(
+                    data.getString(cityIndex));
+            stateTextInputLayout.getEditText().setText(
+                    data.getString(stateIndex));
+            zipTextInputLayout.getEditText().setText(
+                    data.getString(zipIndex));
+
+            updateSaveButtonFAB();
+        }
+    }
+
+    // called by LoaderManager when the Loader is being reset
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) { }
 }
--- app/src/main/java/com/omelchenkoaleks/importantcontacts/DetailFragment.java
+++ app/src/main/java/com/omelchenkoaleks/importantcontacts/DetailFragment.java
@@ -1,108 +1,209 @@
 package com.omelchenkoaleks.importantcontacts;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.omelchenkoaleks.importantcontacts.data.DatabaseDescription;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link DetailFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link DetailFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class DetailFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class DetailFragment extends Fragment
+        implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // callback methods implemented by MainActivity
+    public interface DetailFragmentListener {
+        void onContactDeleted(); // called when a contact is deleted
 
-    private OnFragmentInteractionListener mListener;
-
-    public DetailFragment() {
-        // Required empty public constructor
+        // pass Uri of contact to edit to the DetailFragmentListener
+        void onEditContact(Uri contactUri);
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DetailFragment newInstance(String param1, String param2) {
-        DetailFragment fragment = new DetailFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private static final int CONTACT_LOADER = 0; // identifies the Loader
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private DetailFragmentListener listener; // MainActivity
+    private Uri contactUri; // Uri of selected contact
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false);
-    }
+    private TextView nameTextView; // displays contact's name
+    private TextView phoneTextView; // displays contact's phone
+    private TextView emailTextView; // displays contact's email
+    private TextView streetTextView; // displays contact's street
+    private TextView cityTextView; // displays contact's city
+    private TextView stateTextView; // displays contact's state
+    private TextView zipTextView; // displays contact's zip
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
+    // set DetailFragmentListener when fragment attached
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+        listener = (DetailFragmentListener) context;
     }
 
+    // remove DetailFragmentListener when fragment detached
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+        listener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    // called when DetailFragmentListener's view needs to be created
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        setHasOptionsMenu(true); // this fragment has menu items to display
+
+        // get Bundle of arguments then extract the contact's Uri
+        Bundle arguments = getArguments();
+
+        if (arguments != null)
+            contactUri = arguments.getParcelable(MainActivity.CONTACT_URI);
+
+        // inflate DetailFragment's layout
+        View view =
+                inflater.inflate(R.layout.fragment_detail, container, false);
+
+        // get the EditTexts
+        nameTextView = (TextView) view.findViewById(R.id.nameTextView);
+        phoneTextView = (TextView) view.findViewById(R.id.phoneTextView);
+        emailTextView = (TextView) view.findViewById(R.id.emailTextView);
+        streetTextView = (TextView) view.findViewById(R.id.streetTextView);
+        cityTextView = (TextView) view.findViewById(R.id.cityTextView);
+        stateTextView = (TextView) view.findViewById(R.id.stateTextView);
+        zipTextView = (TextView) view.findViewById(R.id.zipTextView);
+
+        // load the contact
+        getLoaderManager().initLoader(CONTACT_LOADER, null, this);
+        return view;
     }
+
+    // display this fragment's menu items
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_details_menu, menu);
+    }
+
+    // handle menu item selections
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_edit:
+                listener.onEditContact(contactUri); // pass Uri to listener
+                return true;
+            case R.id.action_delete:
+                deleteContact();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // delete a contact
+    private void deleteContact() {
+        // use FragmentManager to display the confirmDelete DialogFragment
+        confirmDelete.show(getFragmentManager(), "confirm delete");
+    }
+
+    // DialogFragment to confirm deletion of contact
+    private final DialogFragment confirmDelete =
+            new DialogFragment() {
+                // create an AlertDialog and return it
+                @Override
+                public Dialog onCreateDialog(Bundle bundle) {
+                    // create a new AlertDialog Builder
+                    AlertDialog.Builder builder =
+                            new AlertDialog.Builder(getActivity());
+
+                    builder.setTitle(R.string.confirm_title);
+                    builder.setMessage(R.string.confirm_message);
+
+                    // provide an OK button that simply dismisses the dialog
+                    builder.setPositiveButton(R.string.button_delete,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(
+                                        DialogInterface dialog, int button) {
+
+                                    // use Activity's ContentResolver to invoke
+                                    // delete on the AddressBookContentProvider
+                                    getActivity().getContentResolver().delete(
+                                            contactUri, null, null);
+                                    listener.onContactDeleted(); // notify listener
+                                }
+                            }
+                    );
+
+                    builder.setNegativeButton(R.string.button_cancel, null);
+                    return builder.create(); // return the AlertDialog
+                }
+            };
+
+    // called by LoaderManager to create a Loader
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // create an appropriate CursorLoader based on the id argument;
+        // only one Loader in this fragment, so the switch is unnecessary
+        CursorLoader cursorLoader;
+
+        switch (id) {
+            case CONTACT_LOADER:
+                cursorLoader = new CursorLoader(getActivity(),
+                        contactUri, // Uri of contact to display
+                        null, // null projection returns all columns
+                        null, // null selection returns all rows
+                        null, // no selection arguments
+                        null); // sort order
+                break;
+            default:
+                cursorLoader = null;
+                break;
+        }
+
+        return cursorLoader;
+    }
+
+    // called by LoaderManager when loading completes
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // if the contact exists in the database, display its data
+        if (data != null && data.moveToFirst()) {
+            // get the column index for each data item
+            int nameIndex = data.getColumnIndex(DatabaseDescription.Contact.COLUMN_NAME);
+            int phoneIndex = data.getColumnIndex(DatabaseDescription.Contact.COLUMN_PHONE);
+            int emailIndex = data.getColumnIndex(DatabaseDescription.Contact.COLUMN_EMAIL);
+            int streetIndex = data.getColumnIndex(DatabaseDescription.Contact.COLUMN_STREET);
+            int cityIndex = data.getColumnIndex(DatabaseDescription.Contact.COLUMN_CITY);
+            int stateIndex = data.getColumnIndex(DatabaseDescription.Contact.COLUMN_STATE);
+            int zipIndex = data.getColumnIndex(DatabaseDescription.Contact.COLUMN_ZIP);
+
+            // fill TextViews with the retrieved data
+            nameTextView.setText(data.getString(nameIndex));
+            phoneTextView.setText(data.getString(phoneIndex));
+            emailTextView.setText(data.getString(emailIndex));
+            streetTextView.setText(data.getString(streetIndex));
+            cityTextView.setText(data.getString(cityIndex));
+            stateTextView.setText(data.getString(stateIndex));
+            zipTextView.setText(data.getString(zipIndex));
+        }
+    }
+
+    // called by LoaderManager when the Loader is being reset
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) { }
 }
